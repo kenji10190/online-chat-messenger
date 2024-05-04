ea9e5a2810e78d8fbe624077848cfe5736295743
kenji10190 <153744023+kenji10190@users.noreply.github.com>
2024-05-04 18:51:45 +0900

    create online chat messenger

---
+++ client.py
@@ -0,0 +1,62 @@
+import socket
+import ntplib
+import time
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+server_address = input("Enter server address: ")
+server_port = 9001
+
+# 0.0.0.0にアドレスを設定することにより、このクライアントのアクティブな
+# ネットワークインターフェースで受信することが可能になる。
+# port番号を0にすることで、osがほかのクライアントと衝突しない
+# port番号を設定してくれる。
+sock.bind(("", 0))
+
+def get_ntp_time():
+    client = ntplib.NTPClient()
+    try:
+        # pool.ntp.orgはntpサーバーの名前、サーバーのversionが3というもの
+        response = client.request("pool.ntp.org", version=3)
+        # timeモジュールのもので、分かりやすい時間表記に変更する
+        return time.ctime(response.tx_time)
+    except Exception as e:
+        return str(e)
+
+user_name = input("Enter your user name: ")
+user_name_length = len(user_name)
+first_message = input("Enter your first message: ")
+time_stamp = get_ntp_time()
+
+try:
+    # 数値もbinaryに変換しないとサーバーが受け取れない
+    sock.sendto(user_name_length.to_bytes(1, byteorder="big") + user_name.encode() + time_stamp.encode() + first_message.encode(), (server_address, server_port))
+except Exception as e:
+    print(str(e))
+
+try:
+    while True:
+        data, address = sock.recvfrom(4096)
+        remote_user_name_length = data[0]
+        remote_user_name = data[1:1+remote_user_name_length].decode()
+        # この間にタイムスタンプが24文字で含まれているため、そのあとからメッセージ取得
+        remote_message = data[1+remote_user_name_length+25:].decode()
+
+        print(f"{remote_user_name}: {remote_message}")
+
+        message = input("メッセージを入力してください。")
+        if "抜けます" in message:
+            break
+
+        time_stamp = get_ntp_time()
+        
+        try:
+            sock.sendto(user_name_length.to_bytes(1, byteorder="big") + user_name.encode() + time_stamp.encode() + message.encode(), (server_address, server_port))
+        except Exception as e:
+            print(str(e))
+
+except KeyboardInterrupt:
+    print("通信を切断します。")
+    
+finally:
+    sock.close()
---
+++ server.py
@@ -0,0 +1,62 @@
+import socket
+import os
+from pathlib import Path
+
+# create UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# 0.0.0.0 is accept any address
+server_address = "0.0.0.0"
+server_port = 9001
+
+# 作成したソケットに自身のIP,PORTをbindする
+sock.bind((server_address, server_port))
+
+# クライアントのアドレスとユーザー名をkey, valueで保持
+clients = {}
+
+try:
+    while True:
+        # recvfrom method can accept all clients and data individually
+        data, address = sock.recvfrom(4096)
+
+        # 元々、PCが10進数を2進数で取り扱っているため、数値に変換しなくてOK
+        user_name_length = data[0]
+        user_name = data[1:1+user_name_length].decode()
+        time_stamp = data[1+user_name_length:25+user_name_length].decode()
+        message = data[25+user_name_length:].decode()
+
+        # クライアント辞書に登録がなければ登録する
+        if address not in clients:
+            clients[address] = {"name" : user_name, "time_stamp" : time_stamp, "inactive_count" : 0}
+            print("New client, {} registered.".format(user_name))
+            sock.sendto(user_name.encode() + " registered.".encode(), address)
+
+        # データ送信してきたクライアントのタイムスタンプと削除候補からリセット
+        clients[address]["time_stamp"] = time_stamp
+        clients[address]["inactive_count"] = 0
+
+        # 削除予定クライアントリストを作成し、削除候補回数が6以上のクライアントを追加する。
+        inactive_clients = [addr for addr, info in clients.items() if info["inactive_count"] >= 6]
+
+        # データ送信していない全クライアントについて削除候補に更新し、
+        # もし、候補回数が6以上になったら、削除リストに追加する。
+        # 満たしていなければ、受け取っているデータを転送する
+        for client_address in clients.keys():
+            if client_address != address:
+                clients[client_address]["inactive_count"] += 1
+                if client_address not in inactive_clients:
+                    sock.sendto(data, client_address)
+
+        # 削除予定リストによってリレーシステムから削除
+        # UDPの場合、クライアントごとにソケットをつないでいる訳ではないため
+        # クライアントリストから削除すればリレーシステムから除外される。
+        for addr in inactive_clients:
+            print(f"Client {clients[addr]['name']} was deleted due to inactivity.")
+            del clients[addr]
+
+except KeyboardInterrupt:
+    print("サーバーを切断します。")
+
+finally:
+    sock.close()
+    
