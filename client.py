e02c75b33063b704952d04c669f81c6485f5639d
kenji10190 <153744023+kenji10190@users.noreply.github.com>
2024-05-08 15:35:15 +0900

    add non-blocking mode

--- client.py
+++ client.py
@@ -34,30 +34,37 @@ try:
 except Exception as e:
     print(str(e))
 
+bufferSize = 4096
+
 try:
     while True:
-        data, address = sock.recvfrom(4096)
+        try:
+            data, address = sock.recvfrom(bufferSize)
 
-        if data.decode().startswith("REGISTERED"):
-            print("You have been registered as:", data.decode().split(" ")[1])
-        else:
-            remote_user_name_length = data[0]
-            remote_user_name = data[1:1+remote_user_name_length].decode()
-            # この間にタイムスタンプが24文字で含まれているため、そのあとからメッセージ取得
-            remote_message = data[1+remote_user_name_length+25:].decode()
+            # もし、サーバーから新規登録通知が来たら、その旨を出力
+            if data.decode().startswith("REGISTERED"):
+                print("You have been registered as:", data.decode().split(" ")[1])
+            else:
+                remote_user_name_length = data[0]
+                remote_user_name = data[1:1+remote_user_name_length].decode()
+                # この間にタイムスタンプが24文字で含まれているため、そのあとからメッセージ取得
+                remote_message = data[1+remote_user_name_length+25:].decode()
 
-            print(f"{remote_user_name}: {remote_message}")
+                print(f"{remote_user_name}: {remote_message}")
 
-            message = input("メッセージを入力してください。")
-            if "抜けます" in message:
-                break
+                message = input("メッセージを入力してください。")
+                if "抜けます" in message:
+                    break
 
-            time_stamp = get_ntp_time()
-            
-            try:
-                sock.sendto(user_name_length.to_bytes(1, byteorder="big") + user_name.encode() + time_stamp.encode() + message.encode(), (server_address, server_port))
-            except Exception as e:
-                print(str(e))      
+                time_stamp = get_ntp_time()
+                
+                try:
+                    sock.sendto(user_name_length.to_bytes(1, byteorder="big") + user_name.encode() + time_stamp.encode() + message.encode(), (server_address, server_port))
+                except Exception as e:
+                    print(str(e))      
+        # データがない場合、再ループ            
+        except BlockingIOError:
+            continue
 
 except KeyboardInterrupt:
     print("通信を切断します。")
--- server.py
+++ server.py
@@ -10,51 +10,64 @@ server_port = 9001
 
 # 作成したソケットに自身のIP,PORTをbindする
 sock.bind((server_address, server_port))
+# ノンブロッキングモードに設定
+sock.setblocking(0);
+# アドレスの再利用。ソケットを切断すると、TIME_WAIT状態になりそのポートを再利用できるまで時間がかかるため、それを防止する。
+# あくまで開発用メソッド
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 # クライアントのアドレスとユーザー名をkey, valueで保持
 clients = {}
+bufferSize = 4096
 
 try:
+    # 何らかの切断サインがない限り、受信ループする
     while True:
-        # recvfrom method can accept all clients and data individually
-        data, address = sock.recvfrom(4096)
+        try:
+            # recvfrom method can accept all clients and data individually
+            data, address = sock.recvfrom(bufferSize)
 
-        # 元々、PCが10進数を2進数で取り扱っているため、数値に変換しなくてOK
-        user_name_length = data[0]
-        user_name = data[1:1+user_name_length].decode()
-        time_stamp = data[1+user_name_length:25+user_name_length].decode()
-        message = data[25+user_name_length:].decode()
-        print(f"{user_name_length}, {user_name}, {time_stamp}, {message}")
+            # 元々、PCが10進数を2進数で取り扱っているため、数値に変換しなくてOK
+            user_name_length = data[0]
+            user_name = data[1:1+user_name_length].decode()
+            time_stamp = data[1+user_name_length:25+user_name_length].decode()
+            message = data[25+user_name_length:].decode()
+            print(f"{user_name_length}, {user_name}, {time_stamp}, {message}")
 
-        # クライアント辞書に登録がなければ登録する
-        if address not in clients:
-            clients[address] = {"name" : user_name, "time_stamp" : time_stamp, "inactive_count" : 0}
-            print("New client, {} registered.".format(user_name))
-            registered_message = "REGISTERED " + user_name
-            sock.sendto(registered_message.encode(), address)
+            # クライアント辞書に登録がなければ登録する
+            if address not in clients:
+                clients[address] = {"name" : user_name, "time_stamp" : time_stamp, "inactive_count" : 0}
+                print("New client, {} registered.".format(user_name))
+                # 新規登録したクライアントに通知を送信
+                registered_message = "REGISTERED " + user_name
+                sock.sendto(registered_message.encode(), address)
 
-        # データ送信してきたクライアントのタイムスタンプと削除候補からリセット
-        clients[address]["time_stamp"] = time_stamp
-        clients[address]["inactive_count"] = 0
+            # データ送信してきたクライアントのタイムスタンプと削除候補からリセット
+            clients[address]["time_stamp"] = time_stamp
+            clients[address]["inactive_count"] = 0
 
-        # 削除予定クライアントリストを作成し、削除候補回数が6以上のクライアントを追加する。
-        inactive_clients = [addr for addr, info in clients.items() if info["inactive_count"] >= 6]
+            # 削除予定クライアントリストを作成し、削除候補回数が6以上のクライアントを追加する。
+            inactive_clients = [addr for addr, info in clients.items() if info["inactive_count"] >= 6]
 
-        # データ送信していない全クライアントについて削除候補に更新し、
-        # もし、候補回数が6以上になったら、削除リストに追加する。
-        # 満たしていなければ、受け取っているデータを転送する
-        for client_address in clients.keys():
-            if client_address != address:
-                clients[client_address]["inactive_count"] += 1
-                if client_address not in inactive_clients:
-                    sock.sendto(data, client_address)
+            # データ送信していない全クライアントについて削除候補に更新し、
+            # もし、候補回数が6以上になったら、削除リストに追加する。
+            # 満たしていなければ、受け取っているデータを転送する
+            for client_address in clients.keys():
+                if client_address != address:
+                    clients[client_address]["inactive_count"] += 1
+                    if client_address not in inactive_clients:
+                        sock.sendto(data, client_address)
 
-        # 削除予定リストによってリレーシステムから削除
-        # UDPの場合、クライアントごとにソケットをつないでいる訳ではないため
-        # クライアントリストから削除すればリレーシステムから除外される。
-        for addr in inactive_clients:
-            print(f"Client {clients[addr]['name']} was deleted due to inactivity.")
-            del clients[addr]
+            # 削除予定リストによってリレーシステムから削除
+            # UDPの場合、クライアントごとにソケットをつないでいる訳ではないため
+            # クライアントリストから削除すればリレーシステムから除外される。
+            for addr in inactive_clients:
+                print(f"Client {clients[addr]['name']} was deleted due to inactivity.")
+                del clients[addr]
+        
+        # データがない場合も、再ループさせる
+        except BlockingIOError:
+            continue
 
 except KeyboardInterrupt:
     print("サーバーを切断します。")
