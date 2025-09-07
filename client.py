4fcf820aa3eadaa5df3f42f2712f71de031fedbd
kenji10190 <oinari13579@gmail.com>
2025-09-07 20:49:02 +0900

    Add threading for concurrent message receiving

--- client.py
+++ client.py
@@ -1,8 +1,8 @@
 import socket
 import ntplib
 import time
-import select
 from datetime import datetime
+import threading
 
 class UDPClient:
 
@@ -13,13 +13,14 @@ class UDPClient:
         # クライアントはbindする必要なし。
         # ノンブロッキングモードの設定
         self.sock.setblocking(0)
+        self.running = True
 
     def get_ntp_time(self):
         # NTPのオブジェクトを作成
         client = ntplib.NTPClient()
         try:
             # pool.ntp.org=NTPサーバー、バージョンが3
-            response = client.request("pool.ntp.org", version=3)
+            response = client.request("ntp.nict.jp", version=3)
             # timeモジュールからctimeで分かりやすい時刻表示に変更
             return datetime.fromtimestamp(response.tx_time)
         except Exception:
@@ -32,38 +33,56 @@ class UDPClient:
 
         try:
             # 数値をバイナリにするにはto_byteを使用する。引数は使用するバイト数とエンディアン形式を指定する。
-            self.sock.sendto(user_name_length.to_bytes(1, "big") + user_name.encode() + time_stamp.encode() + message.encode(), (self.server_address, self.server_port))
+            data = ( 
+                user_name_length.to_bytes(1, "big") + 
+                user_name.encode() + 
+                time_stamp.encode() + 
+                message.encode()
+            ) 
+            self.sock.sendto(data, (self.server_address, self.server_port))
         except Exception as e:
             print(str(e))
 
     def receive_data(self):
         bufferSize = 4096
-        # ソケット監視をselectで行う。サーバーと接続しているソケットをリストに入れる。
-        readable, _, exceptional = select.select([self.sock], [], [self.sock], 1)
 
-        if self.sock in readable:
-            data, _ = self.sock.recvfrom(bufferSize)
+        while self.running:
+            try:
+                data, _ = self.sock.recvfrom(bufferSize)
 
-            if data.decode().startswith("REGISTERED"):
-                print(f"{data.decode().split(' ')[1]} として登録されました。")
-            else:
-                remote_user_name_length = data[0]
-                remote_user_name = data[1:1+remote_user_name_length].decode()
-                # 時刻情報が19文字なので、その後から指定する。
-                remote_message = data[20+remote_user_name_length:].decode()
+                if data.decode().startswith("REGISTERED"):
+                    print(f"\n {data.decode().split(' ')[1]} として登録されました。")
+                    print("メッセージ：", end="", flush=True)
+                else:
+                    remote_user_name_length = data[0]
+                    remote_user_name = data[1:1+remote_user_name_length].decode()
+                    # 時刻情報が19文字なので、その後から指定する。
+                    remote_message = data[20+remote_user_name_length:].decode()
 
-                print(f"{remote_user_name}: {remote_message}")
+                    print(f"\r{" " * 50}\r", end="")
+                    print(f"{remote_user_name}: {remote_message}")
+                    print("メッセージ：", end="", flush=True)
+            except BlockingIOError:
+                time.sleep(0.1)      
+            except Exception as e:
+                # もし、ユーザー自身がアプリから抜けた場合にはエラー出力する必要なし
+                if self.running:
+                    print(f"\n受信エラー: {e}") 
         
-        if self.sock in exceptional:
-            print("ソケットにエラーが発生しました")
-
     def run_client(self):
         user_name = input("ユーザー名を入力してください\n")
+        self.send_data(user_name, "")
+
+        receive_message = threading.Thread(target=self.receive_data())
+        receive_message.daemon = True
+        receive_message.start()
+
+        print("\nチャットを開始します。(終了しますでアプリ終了)")
+        print("-" * 40)
         try:
             while True:
-                    self.receive_data()
                     message = input("メッセージを入力してください\n")
-                    if "抜けます" in message:
+                    if "終了します" in message:
                         print("通信を切断します\n")
                         break
                     self.send_data(user_name, message)
@@ -72,10 +91,11 @@ class UDPClient:
             print("強制終了します\n")
 
         finally:
+            self.running = False
             self.sock.close()
 
 if __name__ == "__main__":
-    server_address = input("サーバーアドレスを入力してください\n")
+    server_address = input("サーバーアドレスを入力してください: ")
     server_port = 9001
     client = UDPClient(server_address, server_port)
     client.run_client()
